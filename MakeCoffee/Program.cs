6724024993a38286407d3abeb581c487fe571267
Andre Santos <andremlsantos@gmail.com>
2021-08-18 13:53:54 +0200

    Initial commit

---
+++ MakeCoffee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MakeCoffee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
